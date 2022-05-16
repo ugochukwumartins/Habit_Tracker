be9938ae80453d9cffb333252332aaf51a76dfe3
ugochukwumartins <uokorocha72@gmail.com>
2022-05-16 16:17:12 +0100

    first commit

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/ui/task/task_completion_ring.dart';
+import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.of(context).primary,
+      body: Center(
+        child: SizedBox(
+          width: 240,
+          child: TaskCompletionRing(),),
+      ),
+    );
+  }
+}
