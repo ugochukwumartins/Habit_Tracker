75e78e624b2b39839b8b8fd685ef4fbb2f1bfe5e
ugochukwumartins <uokorocha72@gmail.com>
2022-05-16 17:45:29 +0100

    added animation controller

---
+++ lib/ui/task/animated_task.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, unused_field
+
+import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/ui/task/task_completion_ring.dart';
+
+class AnimatedTask extends StatefulWidget {
+  AnimatedTask({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedTask> createState() => _AnimatedTaskState();
+}
+
+class _AnimatedTaskState extends State<AnimatedTask>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animatedController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animatedController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 750),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animatedController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TaskCompletionRing(progress: 0.6);
+  }
+}
