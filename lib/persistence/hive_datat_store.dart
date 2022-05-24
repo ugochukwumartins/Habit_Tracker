b49d4318036c2e78a052f635b2097a4a183fd6da
ugochukwumartins <uokorocha72@gmail.com>
2022-05-24 17:46:33 +0100

    reading task using hive local db storage

--- lib/persistence/hive_datat_store.dart
+++ lib/persistence/hive_datat_store.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unawaited_futures
 
+import 'package:flutter/foundation.dart';
 import 'package:habit_tracker_flutter/models/task.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -9,13 +10,20 @@ class HiveDataStore {
   Future<void> init() async {
     await Hive.initFlutter();
     Hive.registerAdapter<Task>(TaskAdapter());
-    Hive.openBox<Task>(tasksBoxName);
+   await Hive.openBox<Task>(tasksBoxName);
   }
 
-  Future<void> createDemoTasks({required List<Task> tasks}) async {
+  Future<void> createDemoTasks({
+    required List<Task> tasks,
+    bool force = false,
+  }) async {
     final box = Hive.box<Task>(tasksBoxName);
-    if (box.isEmpty) {
+    if (box.isEmpty || force) {
+      await box.clear();
       await box.addAll(tasks);
-    }
+    } else {}
+  }
+  ValueListenable<Box<Task>> tasksListenable(){
+    return   Hive.box<Task>(tasksBoxName).listenable();
   }
 }
--- lib/ui/home/task_grid.dart
+++ lib/ui/home/task_grid.dart
@@ -3,12 +3,13 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/models/task.dart';
 import 'package:habit_tracker_flutter/models/task_preset.dart';
 import 'package:habit_tracker_flutter/ui/task/task_with_name.dart';
 
 class TaskGrid extends StatelessWidget {
   TaskGrid({Key? key, required this.tasks}) : super(key: key);
-  final List<TaskPreset> tasks;
+  final List<Task> tasks;
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
